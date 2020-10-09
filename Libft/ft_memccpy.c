46fbce1a6373e4cbb02bfd5da29514ba4088348c
Hyoeun Jang <hyojang@c2r3s7.42seoul.kr>
2020-10-09 18:35:21 +0900

    complete ft_memccpy

--- Libft/ft_memccpy.c
+++ Libft/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/09 16:49:29 by hyojang           #+#    #+#             */
-/*   Updated: 2020/10/09 17:26:06 by hyojang          ###   ########.fr       */
+/*   Updated: 2020/10/09 18:34:12 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,21 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
 	size_t	i;
 	char	*dst2;
+	char	*src2;
 
 	i = 0;
+	src2 = (char *)src;
 	while (*(src2 + i) != c && *(src2 + i) != 0)
 		i++;
-	if (i != 0)
+	i++;
+	if (i <= n)
 	{
-		dst2 = (char *)ft_memcpy(dst, src, n) + 1;
-		return (dst2);
+		dst2 = (char *)ft_memcpy(dst, src, i);
+		return (dst2 + i);
 	}
 	else
+	{
+		dst2 = (char *)ft_memcpy(dst, src, n);
 		return (0);
+	}
 }
