d86fd717b3ca9b27fcf450400528c01eec88a968
Hyoeun Jang <hyojang@c2r3s7.42seoul.kr>
2020-10-09 17:26:52 +0900

    norm modified ft_memccpy

--- Libft/ft_memccpy.c
+++ Libft/ft_memccpy.c
@@ -6,13 +6,13 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/09 16:49:29 by hyojang           #+#    #+#             */
-/*   Updated: 2020/10/09 17:13:39 by hyojang          ###   ########.fr       */
+/*   Updated: 2020/10/09 17:26:06 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memccpy(void *dst, const void *src, int c, size_t n)
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
 	size_t	i;
 	char	*dst2;
@@ -20,7 +20,6 @@ void *ft_memccpy(void *dst, const void *src, int c, size_t n)
 	i = 0;
 	while (*(src2 + i) != c && *(src2 + i) != 0)
 		i++;
-
 	if (i != 0)
 	{
 		dst2 = (char *)ft_memcpy(dst, src, n) + 1;
