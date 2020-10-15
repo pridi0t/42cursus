60762503601aa73a8a89d73845113b7ffca57b4a
Hyoeun Jang <hyojang@c2r9s1.42seoul.kr>
2020-10-15 16:48:56 +0900

    add ft_memchr

--- Libft/ft_memchr.c
+++ Libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/12 18:31:23 by hyojang           #+#    #+#             */
-/*   Updated: 2020/10/12 20:22:32 by hyojang          ###   ########.fr       */
+/*   Updated: 2020/10/15 16:46:16 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,18 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	char			*str;
+	unsigned char	*str;
 	unsigned char	ch;
+	size_t			i;
 
+	str = (unsigned char *)s;
 	ch = (unsigned char)c;
+	i = 0;
+	while (i < n)
+	{
+		if (*(str + i) == ch)
+			return (str + i);
+		i++;
+	}
+	return (0);
 }
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 19:50:00 by hyojang           #+#    #+#             */
-/*   Updated: 2020/10/12 17:53:56 by hyojang          ###   ########.fr       */
+/*   Updated: 2020/10/15 14:53:59 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
