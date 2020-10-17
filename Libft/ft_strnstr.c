ede4b15fe4f11959eb315c844c5cf4026c2f38d7
Hyoeun Jang <hyojang@c5r6s3.42seoul.kr>
2020-10-17 14:48:32 +0900

    add ft_strnstr

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/16 17:30:33 by hyojang           #+#    #+#             */
-/*   Updated: 2020/10/17 00:41:01 by hyojang          ###   ########.fr       */
+/*   Updated: 2020/10/17 14:46:40 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		return ((char *)haystack);
 	if (len == 0 || hlen == 0)
 		return (0);
-	while (i < (len - nlen) && *(haystack + i) != 0)
+	while (i <= (len - nlen) && *(haystack + i) != 0)
 	{
 		if (ft_memcmp((haystack + i), needle, nlen) == 0)
 			return ((char *)(haystack + i));
