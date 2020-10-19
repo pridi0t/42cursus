263a075ef192b47e5e40210ce4402bd41688f643
Hyoeun Jang <hyojang@c2r3s5.42seoul.kr>
2020-10-19 17:28:26 +0900

    add ft_strtrim

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/18 23:12:59 by hyojang           #+#    #+#             */
-/*   Updated: 2020/10/18 23:46:13 by hyojang          ###   ########.fr       */
+/*   Updated: 2020/10/19 17:26:49 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,19 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	start;
-	size_t	end;
+	size_t start;
+	size_t end;
 
 	start = 0;
+	if (ft_strlen(set) == 0)
+		return (ft_strdup(s1));
+	if (ft_strlen(s1) == 0)
+		return (ft_strdup(""));
 	end = ft_strlen(s1) - 1;
-	while (*(s1 + start) == *set && *(s1 + start) != 0)
+	while (ft_strchr(set, *(s1 + start)) != 0\
+			&& *(s1 + start) != 0)
 		start++;
-	while (*(s1 + end) == *set && end >= start)
+	while (ft_strchr(set, *(s1 + end)) != 0 && end >= start)
 		end--;
-	return (ft_substr(s1, start, (start - end + 2)));
+	return (ft_substr(s1, start, (end - start + 1)));
 }
