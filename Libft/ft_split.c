eafc5e000b0a7bf50a9367b0ee8133bdd3c63ba5
Hyoeun Jang <hyojang@c3r3s1.42seoul.kr>
2020-10-22 19:30:43 +0900

    add ft_split

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,11 +6,85 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 20:56:29 by hyojang           #+#    #+#             */
-/*   Updated: 2020/10/21 21:04:22 by hyojang          ###   ########.fr       */
+/*   Updated: 2020/10/22 19:27:53 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+size_t	plen(char *s, char c)
+{
+	size_t	i;
+	size_t	cnt;
+
+	i = 0;
+	cnt = 0;
+	while (*(s + i) != 0)
+	{
+		if (*(s + i) != c && *(s + i) != 0)
+		{
+			cnt++;
+			while (*(s + i) != c && *(s + i) != 0)
+				i++;
+		}
+		while (*(s + i) == c && *(s + i) != 0)
+			i++;
+	}
+	return (cnt);
+}
+
+char	*strin(char *s, char c, size_t *i)
+{
+	size_t	len;
+	char	*mem;
+
+	len = 0;
+	while (*(s + len) != c && *(s + len) != 0)
+		len++;
+	*i += len;
+	mem = ft_substr(s, 0, len);
+	if (mem == 0)
+	{
+		free(mem);
+		return (0);
+	}
+	return (mem);
+}
+
+char	**ncheck(char **mem)
+{
+	mem = (char **)malloc(sizeof(char *) * 1);
+	if (mem == 0)
+		return (0);
+	mem[0] = 0;
+	return (mem);
+}
+
 char	**ft_split(char const *s, char c)
 {
+	size_t	i;
+	int		cnt;
+	char	**mem;
 
+	mem = 0;
+	if (*s == 0 && c == 0)
+		return (ncheck(mem));
+	mem = (char **)malloc(sizeof(char *) * (plen((char *)s, c) + 1));
+	if (mem == 0)
+		return (0);
+	i = 0;
+	cnt = -1;
+	while (*(s + i) != 0)
+	{
+		if (*(s + i) != c && *(s + i) != 0)
+		{
+			mem[++cnt] = strin(((char *)s + i), c, &i);
+			if (mem[cnt] == 0)
+				return (0);
+		}
+		while (*(s + i) == c && *(s + i) != 0)
+			i++;
+	}
+	mem[++cnt] = 0;
+	return (mem);
 }
