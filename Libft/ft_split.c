a9ac055058ceb98989c8310d5358e13fee7f1fc5
Hyoeun Jang <hyojang@c3r3s1.42seoul.kr>
2020-10-22 20:41:15 +0900

    add ft_aplit

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 20:56:29 by hyojang           #+#    #+#             */
-/*   Updated: 2020/10/22 19:53:44 by hyojang          ###   ########.fr       */
+/*   Updated: 2020/10/22 20:37:33 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,7 +83,7 @@ char	**ft_split(char const *s, char c)
 	mem = 0;
 	if (*s == 0 && c == 0)
 		return (ncheck(mem));
-	mem = (char **)ft_calloc(sizeof(char *) * (plen((char *)s, c) + 1));
+	mem = (char **)ft_calloc(sizeof(char *), (plen((char *)s, c) + 1));
 	if (mem == 0)
 		return (0);
 	i = 0;
@@ -92,8 +92,8 @@ char	**ft_split(char const *s, char c)
 	{
 		if (*(s + i) != c && *(s + i) != 0)
 		{
-			mem[++cnt] = strin(mem[cnt], ((char *)s + i), c, &i);
-			if (mem == 0)
+			cnt++;
+			if ((mem[cnt] = strin(mem[cnt], ((char *)s + i), c, &i)) == 0)
 				return (mfree(mem));
 		}
 		while (*(s + i) == c && *(s + i) != 0)
