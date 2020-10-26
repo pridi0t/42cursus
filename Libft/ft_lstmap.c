71d8383383d76644800b1b22f24d86f19b219489
Hyoeun Jang <hyojang@c6r5s5.42seoul.kr>
2020-10-26 20:00:04 +0900

    add ft_lstmap

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 16:34:26 by hyojang           #+#    #+#             */
-/*   Updated: 2020/10/26 17:42:24 by hyojang          ###   ########.fr       */
+/*   Updated: 2020/10/26 19:59:19 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
+	t_list *newt;
+	t_list *tmp;
+
+	newt = 0;
+	if (lst == 0 || f == 0)
+		return (0);
+	while (lst != 0)
+	{
+		tmp = ft_lstnew((*f)(lst->content));
+		if (tmp == 0)
+		{
+			ft_lstclear(&newt, del);
+			return (0);
+		}
+		ft_lstadd_back(&newt, tmp);
+		lst = lst->next;
+	}
+	return (newt);
 }
