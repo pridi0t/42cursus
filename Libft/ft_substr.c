8ead2d222fe7459a3bb2612939853b5ef60f9118
Hyoeun Jang <hyojang@c3r2s7.42seoul.kr>
2020-10-29 14:50:49 +0900

    modified memory leak

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/18 22:29:47 by hyojang           #+#    #+#             */
-/*   Updated: 2020/10/18 22:49:49 by hyojang          ###   ########.fr       */
+/*   Updated: 2020/10/29 14:39:28 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	slen;
 	char	*mem;
 
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
 	slen = ft_strlen(s + start);
 	mem = malloc(len + 1);
 	if (mem == 0)
 		return (0);
-	if (start >= ft_strlen(s))
-		return (ft_strdup(""));
 	if (slen >= len)
 		ft_strlcpy(mem, (s + start), len + 1);
 	else
