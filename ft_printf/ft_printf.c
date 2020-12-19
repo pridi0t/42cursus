50af0d3eddade7987030fd6973a860ac010f8b5e
Hyoeun Jang <hyojang@c1r1s1.42seoul.kr>
2020-12-19 16:22:51 +0900

    save temporarily

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,37 +6,44 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/14 16:25:47 by hyojang           #+#    #+#             */
-/*   Updated: 2020/12/18 18:01:52 by hyojang          ###   ########.fr       */
+/*   Updated: 2020/12/19 16:21:26 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		create_str(char *str[], int *i)
+int	catoi(char *str[], int *i)
 {
-	char *p;
-
-	p = calloc(10, sizeof(char));
-	if (p == 0)
-		return (-1);
-	while (str[*i] != )
-
-
+	char s[20];
+	
+	s = ft_memset(s, 0, sizeof(s));
+	while (str[*i] )
 }
 
-void	set_format(t_format *t, char *str[], int *i)
+int	set_format(t_format *t, char *str[], int *i)
 {
+	int tmp;
+
+	t->flag = 0;
+	t->width = -1;
+	t->precision = -1;
+	t->specifier = 0;
 	if (str[*i] == '-' || str[*i] == '0' || str[*i] == '*')
 	{
 		t->flag = str[*i];
 		(*i)++;
+		if (str[*i] == '0' && t->flag != 0)
+			return (-1);
 	}
-
-
-
+	if (str[*i] >= '1' && str[*i] <= '9')
+	{
+		tmp = catoi(t, str, i);
+		(*i)++;
+	}
+	return (0);
 }
 
-int		ft_printf(const char *str, ...)
+int	ft_printf(const char *str, ...)
 {
 	t_format	t;
 	va_list		p;
@@ -51,7 +58,10 @@ int		ft_printf(const char *str, ...)
 		if (str[i] == '%' && read == 0)
 			read = 1;
 		else if (args[i] == '%' && read == 1)
-			set_format(&t, &str, &(++i));
+		{
+			if (set_format(&t, &str, &(++i)) == -1)
+				return (-1);
+		}
 		else
 			write(1, &args[i], 1);
 		i++;
