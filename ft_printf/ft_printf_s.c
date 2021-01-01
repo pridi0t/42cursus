b79c133de8ffb236dc4cc059a13d53473d491bbe
Hyoeun Jang <hyojang@c3r8s7.42seoul.kr>
2021-01-01 19:39:59 +0900

    temporary completion %s not yet norm..

--- ft_printf/ft_printf_s.c
+++ ft_printf/ft_printf_s.c
@@ -6,7 +6,7 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/27 12:53:11 by hyojang           #+#    #+#             */
-/*   Updated: 2020/12/31 20:48:22 by hyojang          ###   ########.fr       */
+/*   Updated: 2021/01/01 19:38:22 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,19 +36,87 @@ int	print_result(int sign, int space, int len, char *str)
 int	print_s(t_format *t, t_status *s, va_list p)
 {
 	char	*str;
-	int		slen;
 	int		lsort;
 	
 	i = 0;
 	lsort = 0;
 	s->result = 0;
+	str = 0;
 	if (t->flag2 != 0 || (t-> flag1 != '-' && t->flag1 != '*'))
 		return (-1);
-	if (t->flag1 == '*')
+	else if (t->flag1 == '*')
 	{
 		t->width = va_arg(p, int);
 		if (t->width <= 0)
 			return (-1);
+		str = va_arg(p, char *);
+		if (str == 0)
+			return (-1);
+		if (t->width > ft_strlen(str))
+			return (printf_result(0, (t->width - ft_strlen(str)), \
+						ft_strlen(str), str));
+	}
+	str = va_arg(p, char *);
+	if (str == 0)
+	{
+		write(1, "", 1);
+		return (0);
+	}
+	else if (t->width != -1 && t->dot == 0)
+	{
+		if (t->width < ft_strlen(str))
+		{
+			write(1, str, ft_strlen(str));
+			return (ft_strlen(str));
+		}
+		if (t->flag1 == '-')
+			return (printf_result(-1, (t->width - ft_strlen(str)), \
+						ft_strlen(str), str));
+		return (printf_result(0, (t->width - ft_strlen(str)), \
+						ft_strlen(str), str));
+	}
+	else if (t->width != -1 && t->dot == 1)
+	{
+		write(1, str, t->width);
+		return (t->width);
+	}
+	else if (t->width == -1 && t->precision != -1)
+	{
+		if (t->width > ft_strlen(str))
+		{
+			write(1, str, ft_strlen);
+			return (ft_strlen);
+		}
+		write(1, str, t->precision);
+		return (t->precision);
+	}
+	else if (t->width != -1 && t->precision != -1)
+	{
+		if (t->width < t->precision)
+		{
+			write(1, str, ft_strlen(str));
+			return (ft_strlen(str));
+		}
+		else if (t->width > t->precision)
+		{
+			if (t->flag1 == '-1')
+				return (print_result(-1, (t->width - t->precision), \
+							t->precision, str));
+			return (print_result(0, (t->width - t->precision), \
+						t->precision, str));
+		}
+		if (t->width > ft_strlen(str))
+			return (print_result(-1, (t->width - ft_strlen(str)), \
+						ft_strlen(str), str));
+		write(1, str, t->width);
+		return (t->width);
+	}
+	else if (t->width == -1 && t->precision == -1)
+	{
+		if (t->flag1 == '-' && t->dot == 0)
+			return (print_result(0, ft_strlen(str), str));
+		write(1, "", 1);
+		return (0);
 	}
 	return (0);
 }
