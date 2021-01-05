5607ff2dc246ce2db790b30ed1c272f654aca850
Hyoeun Jang <hyojang@c1r2s6.42seoul.kr>
2021-01-05 20:06:42 +0900

    80% complete %s... not yet null

--- ft_printf/ft_printf_s.c
+++ ft_printf/ft_printf_s.c
@@ -6,7 +6,7 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/27 12:53:11 by hyojang           #+#    #+#             */
-/*   Updated: 2021/01/02 20:55:32 by hyojang          ###   ########.fr       */
+/*   Updated: 2021/01/05 20:04:34 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,22 +44,27 @@ int	print_s(t_format *t, va_list p)
 	if (t->precision == -1)
 		t->precision = 0;
 	str = va_arg(p, char *);
-	if (t->precision >= (int)ft_strlen(str))
+	if (t->precision == 0 && t->dot == 0 && (t->width <= (int)ft_strlen(str)))
+		return (s_result(t, 0, ft_strlen(str), str));
+	if (t->precision == 0 && t->dot == 0 && (t->width > (int)ft_strlen(str)))
+		return (s_result(t, (t->width - ft_strlen(str)), ft_strlen(str), str));
+	if (t->width == 0 && t->dot == 1 && (t->precision > (int)ft_strlen(str)))
+		return (s_result(t, 0, ft_strlen(str), str));
+	if (t->precision > (int)ft_strlen(str))
 	{
-		if (t->width > (int)ft_strlen(str))
+		if (t->width < (int)ft_strlen(str))
+			return (s_result(t, 0, ft_strlen(str), str));
 			return (s_result(t, (t->width - ft_strlen(str)), \
 						ft_strlen(str), str));
-			return (s_result(t, 0, t->width, str));
 	}
-	else if (t->precision < (int)ft_strlen(str))
+	else if (t->precision <= (int)ft_strlen(str))
 	{
-		if (t->dot == 0 && (t->width > (int)ft_strlen(str)))
-			return (s_result(t, (t->width - ft_strlen(str)), \
-						ft_strlen(str), str));
-		else if (t->dot == 1 && t->width == 0))
-			return (s_result(t, 0, t->precision, str));
-		else if (t->width > t->precision)
+		if (t->dot == 1 && (t->width > t->precision))
 			return (s_result(t, (t->width - t->precision), t->precision, str));
+		if (t->dot == 1 && (t->width <= t->precision))
+			return (s_result(t, 0, t->precision, str));
+		else if (t->dot == 1 && t->width == 0 && t->precision != 0)
+			return (s_result(t, 0, t->precision, str));
 	}
 	return (s_result(t, 0, ft_strlen(str), str));
 }
