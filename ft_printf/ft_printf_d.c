68ea67a717372b7dc9d528049686c737943c53a8
Hyoeun Jang <hyojang@c2r2s2.42seoul.kr>
2021-01-12 11:22:09 +0900

    complete %d ^0^

--- ft_printf/ft_printf_d.c
+++ ft_printf/ft_printf_d.c
@@ -6,13 +6,13 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 12:31:22 by hyojang           #+#    #+#             */
-/*   Updated: 2021/01/12 09:59:51 by hyojang          ###   ########.fr       */
+/*   Updated: 2021/01/12 11:21:44 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int prtc(int n, char c)
+int	prtc(int n, char c)
 {
 	int i;
 
@@ -23,6 +23,76 @@ int prtc(int n, char c)
 		i++;
 	}
 	return (i);
+}
+
+int	d_ps(t_format *t, char *str, int *num)
+{
+	int result;
+
+	result = 0;
+	if (*num < 0)
+	{
+		write(1, "-", 1);
+		str++;
+	}
+	result += prtc(t->precision - ft_strlen(str), '0');
+	write(1, str, ft_strlen(str));
+	result += ft_strlen(str);
+	return (result);
+}
+
+int	d_wps(t_format *t, char *str, int *num)
+{
+	if (t->flag == '-')
+	{
+		if (*num < 0)
+		{
+			write(1, "-", 1);
+			str++;
+			t->width--;
+		}
+		prtc(t->precision - ft_strlen(str), '0');
+		write(1, str, ft_strlen(str));
+		prtc(t->width - t->precision, ' ');
+		return (t->width);
+	}
+	if (*num < 0)
+		t->width--;
+	prtc(t->width - t->precision, ' ');
+	if (*num < 0)
+	{
+		write(1, "-", 1);
+		str++;
+	}
+	prtc(t->precision - ft_strlen(str), '0');
+	write(1, str, ft_strlen(str));
+	return (t->width);
+}
+
+int	d_wsp(t_format *t, char *str, int *num)
+{
+	int result;
+
+	result = 0;
+	if (t->flag == '0' && t->precision != 0)
+		t->flag = 0;
+	if (t->flag == '-')
+		write(1, str, ft_strlen(str));
+	if (t->flag == '0')
+	{
+		if (*num < 0)
+		{
+			write(1, "-", 1);
+			str++;
+			t->width--;
+		}
+		result += prtc(t->width - ft_strlen(str), '0');
+	}
+	else
+		result += prtc(t->width - ft_strlen(str), ' ');
+	if (t->flag != '-')
+		write(1, str, ft_strlen(str));
+	return (t->width);
 }
 
 int	print_d(t_format *t, va_list p)
@@ -38,70 +108,12 @@ int	print_d(t_format *t, va_list p)
 		str = "";
 	if (t->precision > (int)ft_strlen(str))
 	{
-		// precision > strlen, (ignore width)
 		if (t->precision > t->width)
-		{
-			if (num < 0)
-			{
-				write(1, "-", 1);
-				str++;
-			}
-			result += prtc(t->precision - ft_strlen(str), '0');
-			write(1, str, ft_strlen(str));
-			result += ft_strlen(str);
-			return (result);
-		}
-		// width > precision > strlen
-		if (t->flag == '-')
-		{
-			if (num < 0)
-			{
-				write(1, "-", 1);
-				str++;
-				t->width--;
-			}
-			prtc(t->precision - ft_strlen(str), '0');
-			write(1, str, ft_strlen(str));
-			prtc(t->width - t->precision, ' ');
-		}
-		if (t->flag != '-')
-		{
-			if (num < 0)
-				t->width--;
-			prtc(t->width - t->precision, ' ');
-			if (num < 0)
-			{
-				write(1, "-", 1);
-				str++;
-			}
-			prtc(t->precision - ft_strlen(str), '0');
-			write(1, str, ft_strlen(str));
-		}
-		return (t->width);
+			return (d_ps(t, str, &num));
+		return (d_wps(t, str, &num));
 	}
 	else if (t->width > (int)ft_strlen(str))
-	{
-		// width > strlen > precision
-		if (t->flag == '-')
-			write(1, str, ft_strlen(str));
-		if (t->flag == '0' && t->precision != 0)
-			t->flag = 0;
-		if (t->flag == '0')
-		{
-			if (num < 0)
-			{
-				write(1, "-", 1);
-				str++;
-				t->width--;
-			}
-			result += prtc(t->width - ft_strlen(str), '0');
-		}
-		else
-			result += prtc(t->width - ft_strlen(str), ' ');
-		if (t->flag != '-')
-			write(1, str, ft_strlen(str));
-		return (t->width);
-	}
+		return (d_wsp(t, str, &num));
 	write(1, str, ft_strlen(str));
 	result += ft_strlen(str);
 	return (result);
