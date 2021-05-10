d696913b393d1d25c621861bdc9fd0901a722da2
Hyoeun Jang <hyojang@c4r3s4.42seoul.kr>
2021-05-11 03:31:21 +0900

    remove auto enter...

--- settings/guacamoleSetting.sh
+++ settings/guacamoleSetting.sh
@@ -8,6 +8,7 @@ echo "set nu" >> ~/.vimrc
 echo "set autoindent" >> ~/.vimrc
 echo "set cindent" >> ~/.vimrc
 echo "set showmatch" >> ~/.vimrc
+echo "set nofixendofline" >> ~/.vimrc
 export MAIL="hyojang@student.42seoul.kr"
 alias cc="gcc -Werror -Wall -Wextra"
 alias norm="norminette"
