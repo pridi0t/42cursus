770c33c393dd037e3080000c8e98156a35edbc28
Hyoeun Jang <hyojang@c7r2s10.42seoul.kr>
2021-09-18 15:22:39 +0900

    remove 42header, add brew(for minishell)

--- settings/guacamoleSetting.sh
+++ settings/guacamoleSetting.sh
@@ -14,16 +14,20 @@ alias cc="gcc -Werror -Wall -Wextra"
 alias norm="norminette"
 
 # Stdheader
-mkdir -p ~/.vim/plugin
-cp stdheader.vim ~/.vim/plugin/
-if [ ! -z "$USER" ]
-then
-    echo "USER=`/usr/bin/whoami`" >> ~/.zshrc
-    echo "export USER" >> ~/.zshrc
-fi
+#mkdir -p ~/.vim/plugin
+#cp stdheader.vim ~/.vim/plugin/
+#if [ ! -z "$USER" ]
+#then
+#    echo "USER=`/usr/bin/whoami`" >> ~/.zshrc
+#    echo "export USER" >> ~/.zshrc
+#fi
+#
+#if [ ! -z "$GROUP" ]
+#then
+#    echo "GROUP=`/usr/bin/id -gn $user`" >> ~/.zshrc
+#    echo "export GROUP" >> ~/.zshrc
+#fi
 
-if [ ! -z "$GROUP" ]
-then
-    echo "GROUP=`/usr/bin/id -gn $user`" >> ~/.zshrc
-    echo "export GROUP" >> ~/.zshrc
-fi
+#brew
+rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && echo 'export PATH=$HOME/.brew/bin:$PATH' >> $HOME/.zshrc && source $HOME/.zshrc && brew update
+brew install readline
