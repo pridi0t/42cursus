503945d7563ba36e0d1d5f6b5085ac753623ca5c
Hyoeun Jang <hyojang@c1r8s7.42seoul.kr>
2021-12-30 22:05:45 +0900

    header email k -> kr

--- CPP_Module/CPP_Module_01/ex02/main.cpp
+++ CPP_Module/CPP_Module_01/ex02/main.cpp
@@ -5,24 +5,9 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/29 17:38:20 by hyojang           #+#    #+#             */
-/*   Updated: 2021/12/30 22:04:24 by hyojang          ###   ########.fr       */
+/*   Created: 2021/12/30 22:01:45 by hyojang           #+#    #+#             */
+/*   Updated: 2021/12/30 22:01:53 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
 
-int main()
-{
-	int		count;
-	Zombie	*zom_arr;
-
-	std::cout << "N : ";
-	std::cin >> count;
-
-	zom_arr = zombieHorde(count, "zombie");
-	std::cout << "zom_arr = " << zom_arr << std::endl;
-	delete [] zom_arr;
-
-	return (0);
-}
