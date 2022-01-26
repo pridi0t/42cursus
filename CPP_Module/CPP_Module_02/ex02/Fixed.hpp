c2ecebd99e95f53cbc0871e64a5a4e08e97e9d1c
Hyoeun Jang <hyojang@c4r4s6.42seoul.kr>
2022-01-26 20:11:22 +0900

    CPP_02/ex02 ~ing

--- CPP_Module/CPP_Module_02/ex02/Fixed.hpp
+++ CPP_Module/CPP_Module_02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 15:11:42 by hyojang           #+#    #+#             */
-/*   Updated: 2022/01/26 19:39:13 by hyojang          ###   ########.fr       */
+/*   Updated: 2022/01/26 20:10:56 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,44 @@ class Fixed {
 		static const int fbit = 8;
 
 	public:
+		// constructor
 		Fixed();
 		Fixed(const int ivalue);
 		Fixed(const float fvalue);
 		Fixed(const Fixed& fix);
+
+		// assignation operator overload
 		Fixed& operator=(const Fixed &fix);
+
+		// comparision operators
+		// >
+		// <
+		// >=
+		// <=
+		// ==
+		// !=
+
+		// arithmetic operators
+		// +
+		// -
+		// *
+		// /
+
+		// etc
+		// ++f
+		// f++
+		// --f
+		// f--
+
 		~Fixed();
 
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
 		float	toFloat(void) const;
 		int		toInt(void) const;
+		// publit static member functions overloads
+		// min??
+		// max
 };
 
 std::ostream& operator<<(std::ostream& os, const Fixed &fix);
