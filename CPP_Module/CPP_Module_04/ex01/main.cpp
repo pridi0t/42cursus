f9cb3fe46cced6d594fc52fbd4da87947b245c80
Hyoeun Jang <hyojang@c6r4s6.42seoul.kr>
2022-02-08 07:25:26 +0900

    CPP_04/ex01 edit main.cpp

--- CPP_Module/CPP_Module_04/ex01/main.cpp
+++ CPP_Module/CPP_Module_04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 22:57:48 by hyojang           #+#    #+#             */
-/*   Updated: 2022/02/08 06:37:37 by hyojang          ###   ########.fr       */
+/*   Updated: 2022/02/08 07:24:08 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,61 +16,58 @@
 
 int main(void)
 {
-	Dog *a = new Dog();
-	Dog *b = new Dog(*a);
+	// subject
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
 
-	std::cout << a->getType() << std::endl;
-	std::cout << b->getType() << std::endl;
-	a->setBr(0, "a");
-	std::cout << "========== A ==========" << std::endl;
-	a->printBrain();
+	delete j;
+	delete i;
 
-	std::cout << std::endl << "========== B ==========" << std::endl;
-	b->printBrain();
+	// test
+	std::cout << "========== create ==========" << std::endl;
+	Animal *a[10];
 
-	*a = *b;
-
-	b->setBr(0, "b");
-	std::cout << "========== A ==========" << std::endl;
-	a->printBrain();
-
-	std::cout << std::endl << "========== B ==========" << std::endl;
-	b->printBrain();
-	delete a;
-	delete b;
-
-/*
-	Animal *a = new Animal[5];
-
-	for (int i = 0 ; i < 5 ; i++)
-	{
+	for (int i = 0 ; i < 2 ; i++)
+		a[i] = new Dog();
+	for (int i = 2 ; i < 4 ; i++)
 		a[i] = new Cat();
-		std::cout << a[i]->getType() << std::endl;
-	}
-	
-	delete[] a;
-*/
 
-	/*
-	Brain a;
-	Brain b(a);
+	std::cout << "========== before ==========" << std::endl;
+	Dog *da;
+	Cat *ca;
+	for (int i = 0 ; i < 2 ; i++)
+	{
+		da = dynamic_cast<Dog *>(a[i]);
+		da->printBrain();
+		std::cout << std::endl;
+	}
+	for (int i = 2 ; i < 4 ; i++)
+	{
+		ca = dynamic_cast<Cat *>(a[i]);
+		ca->printBrain();
+		std::cout << std::endl;
+	}
 
-	a.setBr(0, "a");
-	std::cout << "========== A ==========" << std::endl;
-	for (int i = 0 ; i < 100 ; i++)
+	da->setBr(0, "a");
+	ca->setBr(0, "b");
+
+	std::cout << "========== after ==========" << std::endl;
+	for (int i = 0 ; i < 2 ; i++)
 	{
-		std::cout << a.getBr(i) << " ";
-		if (i % 10 == 9)
-			std::cout << std::endl;
+		da = dynamic_cast<Dog *>(a[i]);
+		da->printBrain();
+		std::cout << std::endl;
 	}
-	
-	std::cout << "========== B ==========" << std::endl;
-	for (int i = 0 ; i < 100 ; i++)
+	for (int i = 2 ; i < 4 ; i++)
 	{
-		std::cout << b.getBr(i) << " ";
-		if (i % 10 == 9)
-			std::cout << std::endl;
+		ca = dynamic_cast<Cat *>(a[i]);
+		ca->printBrain();
+		std::cout << std::endl;
 	}
-	*/
+
+	// delete
+	for (int i = 0 ; i < 4 ; i++)
+		delete a[i];
+
 	return 0;
 }
