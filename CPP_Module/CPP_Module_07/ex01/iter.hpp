4f3a0a97e31f35c107e878e6fb5e2f04c2e9a345
Hyoeun Jang <hyojang@student.42seoul.kr>
2022-02-15 14:04:08 +0000

    CPP_07/ex01 ~ing

---
+++ CPP_Module/CPP_Module_07/ex01/iter.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/15 13:57:29 by marvin            #+#    #+#             */
+/*   Updated: 2022/02/15 14:03:38 by marvin           ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef ITER_HPP
+# define ITER_HPP
+
+template <typename T, typename Func = T()()>
+void swap(T *arr, int len, Func func)
+{
+}
+
+# endif
