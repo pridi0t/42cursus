579804ab25526464ee587a687c026a64dfff5694
Hyoeun Jang <hyojang@student.42seoul.kr>
2022-02-15 13:56:10 +0000

    CPP_07/ex00 done

---
+++ CPP_Module/CPP_Module_07/ex00/Whatever.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/15 13:36:05 by marvin            #+#    #+#             */
+/*   Updated: 2022/02/15 13:43:09 by marvin           ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+template <typename T>
+void swap(T &n1, T &n2)
+{
+    T tmp;
+    tmp = n1;
+    n1 = n2;
+    n2 = tmp;
+}
+
+template <typename T>
+T min(T n1, T n2)
+{
+    if (n1 < n2)
+        return (n1);
+    return (n2);
+}
+
+template <typename T>
+T max(T n1, T n2)
+{
+    if (n1 > n2)
+        return (n1);
+    return (n2);   
+}
+
+# endif
---
+++ CPP_Module/CPP_Module_07/ex00/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/15 13:43:21 by marvin            #+#    #+#             */
+/*   Updated: 2022/02/15 13:54:17 by marvin           ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Whatever.hpp"
+
+int main(void) {
+
+    int a = 2;
+    int b = 3;
+    
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+    std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+    std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+
+    return 0;
+}
