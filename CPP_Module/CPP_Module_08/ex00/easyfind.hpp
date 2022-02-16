2ca8bcdbb6585729d149c872ca909e4473263870
Hyoeun Jang <hyojang@student.42seoul.kr>
2022-02-16 03:39:59 +0000

    add CPP08/ex00

---
+++ CPP_Module/CPP_Module_08/ex00/easyfind.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 01:54:58 by marvin            #+#    #+#             */
+/*   Updated: 2022/02/16 03:39:41 by marvin           ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+template <typename T>
+typename T::iterator easyfind (T container, int i)
+{
+    
+}
+
+# endif
