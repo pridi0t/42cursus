e22b673d6bcae4d0ef560d44c9036c2e040a3101
Hyoeun Jang <hyojang@c4r6s3.42seoul.kr>
2022-02-16 19:50:39 +0900

    edit main CPP_07/ex01

--- CPP_Module/CPP_Module_07/ex01/main.cpp
+++ CPP_Module/CPP_Module_07/ex01/main.cpp
@@ -6,11 +6,25 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 13:57:44 by hyojang           #+#    #+#             */
-/*   Updated: 2022/02/16 17:19:52 by hyojang          ###   ########.fr       */
+/*   Updated: 2022/02/16 19:49:40 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
+
+class Test {
+	private:
+		int num;
+	public:
+		Test() { this->num = 1; };
+		int getNum() const { return (this->num); }
+};
+
+std::ostream & operator << (std::ostream &o, const Test &t)
+{
+	o << t.getNum() << " ";
+	return (o);
+}
 
 template <typename T>
 void print(T x)
@@ -20,17 +34,20 @@ void print(T x)
 
 int main(void)
 {
-    int tab[] = {0, 1, 2, 3, 4};
-    iter(tab, 5, print);
+    int a[] = {0, 1, 2, 3, 4};
+    iter(a, 5, print);
 	std::cout << std::endl;
     
-	char a[] = {'a', 'b'};
-    iter (a, 2, print);
-	std::cout << std::endl;
-    
-	double b[] = {1.2, 2.3};
+	char b[] = {'a', 'b'};
     iter (b, 2, print);
 	std::cout << std::endl;
+    
+	double c[] = {1.2, 2.3};
+    iter (c, 2, print);
+	std::cout << std::endl;
+
+	Test d[10];
+	iter (d, 10, print);
     
     return 0;
 }
