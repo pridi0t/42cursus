8ffd31ac4542147cb94285be4e42f0461b210098
Hyoeun Jang <hyojang@c6r4s6.42seoul.kr>
2022-02-18 00:06:38 +0900

    CPP_08/ex00 done

--- CPP_Module/CPP_Module_08/ex00/easyfind.hpp
+++ CPP_Module/CPP_Module_08/ex00/easyfind.hpp
@@ -6,17 +6,25 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 01:54:58 by hyojang           #+#    #+#             */
-/*   Updated: 2022/02/16 14:25:00 by hyojang          ###   ########.fr       */
+/*   Updated: 2022/02/17 22:23:46 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+#include <algorithm>
+
 template <typename T>
-typename T::iterator easyfind (T container, int i)
+typename T::iterator easyfind (T &container, int i)
 {
-    
+	typename T::iterator iter;
+	
+	iter = std::find(container.begin(), container.end(), i);
+	if (iter == container.end()) {
+		throw std::exception();
+	}
+	return (iter);
 }
 
 # endif
--- CPP_Module/CPP_Module_08/ex00/main.cpp
+++ CPP_Module/CPP_Module_08/ex00/main.cpp
@@ -6,14 +6,58 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 01:55:18 by hyojang           #+#    #+#             */
-/*   Updated: 2022/02/16 14:25:07 by hyojang          ###   ########.fr       */
+/*   Updated: 2022/02/18 00:06:03 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+#include <iostream>
+#include <vector>
+#include <set>
 
 int main()
 {
-    
+	// Vector test
+	std::cout << "---------- vector ----------" << std::endl;
+	std::vector<int> a;
+	for (int i = 0 ; i < 5 ; i++)
+		a.push_back(i);
+
+	std::cout << "[vector] : ";
+	for (std::vector<int>::iterator i = a.begin() ; i < a.end() ; i++)
+		std::cout << *i << " ";
+	std::cout << std::endl;
+
+	try {
+		std::vector<int>::iterator iter1;
+		iter1 = easyfind(a, 1);
+		std::cout << "easyfind(a, 1) : " << *iter1 << std::endl;
+		iter1 = easyfind(a, 5);
+		std::cout << "easyfind(a, 5) : " << *iter1 << std::endl;
+	} catch (std::exception & e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	// Set test
+	std::cout << "----------- set ------------" << std::endl;
+	std::set<int> b;
+	for (int i = 0 ; i < 5 ; i++)
+		b.insert(i);
+
+	std::cout << "[set] : ";
+	for (std::set<int>::iterator i = b.begin() ; i != b.end() ; i++)
+		std::cout << *i << " ";
+	std::cout << std::endl;
+
+	try {
+		std::set<int>::iterator iter2;
+		iter2 = easyfind(b, 2);
+		std::cout << "easyfind(b, 2) : " << *iter2 << std::endl;
+		iter2 = easyfind(b, -1);
+		std::cout << "easyfind(b, -1) : " << *iter2 << std::endl;
+	} catch (std::exception & e) {
+		std::cerr << e.what() << std::endl;
+	}
+
     return 0;
 }
