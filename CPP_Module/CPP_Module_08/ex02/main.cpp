36c51a1c5f534007b6bdabe25b265fcf7535d5a4
Hyoeun Jang <hyojang@c6r4s6.42seoul.kr>
2022-02-18 10:06:14 +0900

    add CPP_08/ex02

---
+++ CPP_Module/CPP_Module_08/ex02/main.cpp
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/18 09:13:38 by hyojang           #+#    #+#             */
+/*   Updated: 2022/02/18 10:02:39 by hyojang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <list>
+
+int main()
+{
+	std::cout << "=========== std::list ===========" << std::endl;
+	std::list<int> lst;
+	
+	lst.push_back(5);
+	lst.push_back(17);
+
+	std::cout << lst.back() << std::endl;
+
+	lst.pop_back();
+
+	std::cout << lst.size() << std::endl;
+
+	lst.push_back(3);
+	lst.push_back(5);
+	lst.push_back(737);
+	lst.push_back(0);
+
+	std::list<int>::iterator it = lst.begin();
+	std::list<int>::iterator ite = lst.end();
+
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::list<int> l(lst);
+	
+	/*
+	std::cout << "========== MutantStack ==========" << std::endl;
+	MutantStack<int> mstack;
+	
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+
+	MutantStack<int>::iterator it2 = mstack.begin();
+	MutantStack<int>::iterator ite2 = lst.end();
+
+	++it2;
+	--it2;
+	while (it2 != ite2)
+	{
+		std::cout << *it2 << std::endl;
+		++it2;
+	}
+	std::stack<int> s(mstack);
+	*/
+	return 0;
+}
