c584b7b1f8b2a1f9c4c1014eb92f0de984a7b744
Hyoeun Jang <hyojang@c6r4s6.42seoul.kr>
2022-02-18 09:08:49 +0900

    CPP_08/ex01 done

--- CPP_Module/CPP_Module_08/ex01/main.cpp
+++ CPP_Module/CPP_Module_08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hyojang <hyojang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 00:24:31 by hyojang           #+#    #+#             */
-/*   Updated: 2022/02/18 04:46:16 by hyojang          ###   ########.fr       */
+/*   Updated: 2022/02/18 09:07:53 by hyojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,35 +43,38 @@ int main(void)
 
 	// exception
 	std::cout << "======== exception ========" << std::endl;
+	std::cout << "1) If the number of elements is 1" << std::endl;
 	try {
 		Span sp4(1);
 		sp4.addNumber(1);
-		//sp4.shortestSpan();
-		//sp4.longestSpan();
+		//std::cout << sp4.shortestSpan() << std::endl;
+		//std::cout << sp4.longestSpan() << std::endl;
 	} catch (std::exception & e) {
 		std::cerr << e.what() << std::endl;
 	}
 
-	std::cout << "======== exception ========" << std::endl;
+	std::cout << "2) If storage capacity is exceeded" << std::endl;
 	try {
 		sp.addNumber(5);
 		sp.addNumber(20);
 	} catch (std::exception & e) {
 		std::cerr << e.what() << std::endl;
 	}
-/*
+
 	// etc
-	try {
-		Span sp4(10000);
-		srand((unsigned int)time(NULL));
-		while (sp4.getArr().size() <= 10000)
-		{
-			sp4.addNumber((int)rand() % 20000);
-		}
-		std::cout << sp4 << std::endl;
-	} catch (std::exception & e) {
-		std::cerr << e.what() << std::endl;
+	/*
+	Span sp4(10000);
+	int num;
+
+	srand((unsigned int)time(NULL));
+	while (sp4.getArr().size() < 10000)
+	{
+		num = (rand() % 20000);
+		sp4.addNumber(num);
 	}
-*/
+	std::cout << sp4 << std::endl;
+	std::cout << sp4.shortestSpan() << std::endl;
+	std::cout << sp4.longestSpan() << std::endl;
+	*/
 	return 0;
 }
