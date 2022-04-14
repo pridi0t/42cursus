48bec6baf6ea6c1fdbdd169309b6e2de19b0937f
hyojang <hyojang@student.42seoul.kr>
2022-04-14 12:54:40 +0900

    done mariadb

--- inception/srcs/requirements/mariadb/conf/mariadb_settings.sql
+++ inception/srcs/requirements/mariadb/conf/mariadb_settings.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE wordpress_db;
-GRANT ALL ON wordpress_db.* TO 'mariadb_user'@'localhost' IDENTIFIED BY 'password';
-set password for 'root'@'localhost' = password('root');
+GRANT ALL ON wordpress_db.* TO 'wordpress_user'@'%' IDENTIFIED BY 'password';
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
+/*set password for 'root'@'localhost' = password('root');*/
 flush privileges;
