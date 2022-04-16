69d5905122afb171bcff4bd455f77ed4f74e92d4
hyojang <hyojang@student.42seoul.kr>
2022-04-16 16:04:02 +0900

    yesterday ....T.T

--- inception/srcs/requirements/mariadb/conf/mariadb_settings.sql
+++ inception/srcs/requirements/mariadb/conf/mariadb_settings.sql
@@ -1,5 +1,4 @@
 CREATE DATABASE wordpress_db;
 GRANT ALL ON wordpress_db.* TO 'wordpress_user'@'%' IDENTIFIED BY 'password';
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
-/*set password for 'root'@'localhost' = password('root');*/
 flush privileges;
