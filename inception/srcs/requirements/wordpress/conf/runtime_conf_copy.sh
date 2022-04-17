65b03294f283a4011406689db0124b45a66358ea
hyojang <hyojang@student.42seoul.kr>
2022-04-17 19:13:06 +0900

    modified configure

--- inception/srcs/requirements/wordpress/conf/runtime_conf_copy.sh
+++ inception/srcs/requirements/wordpress/conf/runtime_conf_copy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-FILE="/var/www/html/wordpress"
+FILE="/var/www/html"
 
-if [ -e $FILE/index.php ]; then
-	mv /tmp/wordpress/* $FILE
+if [ ! -e $FILE/index.php ]; then
+	mv /tmp/wordpress $FILE
 fi
 
 exec /usr/sbin/php-fpm7.3 -F -R
