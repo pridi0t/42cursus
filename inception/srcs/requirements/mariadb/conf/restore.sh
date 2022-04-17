70c86f7428dd24bfa4f85834144a3b4ba41169c1
hyojang <hyojang@student.42seoul.kr>
2022-04-18 08:58:46 +0900

    ing

--- inception/srcs/requirements/mariadb/conf/restore.sh
+++ inception/srcs/requirements/mariadb/conf/restore.sh
@@ -1,7 +1,14 @@
-sleep 5s
+sed "s/MD_DBNAME/${MD_DBNAME}/g" /mariadb_settings.sql > /tmp1
+sed "s/MD_ID/${MD_ID}/g" /tmp1 > /tmp2
+sed "s/MD_PW/${MD_PW}/g" /tmp2 > /converted_mariadb_settings.sql
+rm /tmp1 /tmp2
+
+sleep 9s
 
 if [ -e /db/db_backup.sql ]; then
 	mysql < /db/db_backup.sql
+else
+	mysql < /converted_mariadb_settings.sql
 fi
 
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';" | mysql
--- inception/srcs/requirements/wordpress/conf/runtime_conf_copy.sh
+++ inception/srcs/requirements/wordpress/conf/runtime_conf_copy.sh
@@ -3,10 +3,11 @@
 FILE="/var/www/html/wordpress"
 
 if [ ! -e $FILE/index.php ]; then
+	cp -r /wordpress/* $FILE
 	cd /var/www/html/wordpress
-	wp core download --allow-root
-	wp config create --dbname="${MD_DBNAME}" --dbuser="wordpress_user${MD_ID}" --dbpass="${MD_PW}" --dbhost="${MD_HOST}" --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-	wp core install --url="https://hyojang.42.fr" --title="inception" --admin_user="${MD_ADMIN_ID}" --admin_password="${MD_ADMIN_PW}" --admin_email="hyojang@test.com" --skip-email --allow-root
+	sleep 14s;
+	wp config create --dbname="${MD_DBNAME}" --dbuser="${MD_ID}" --dbpass="${MD_PW}" --dbhost="${MD_HOST}" --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+	wp core install --url="https://hyojang.42.fr" --title="inception" --admin_user="${WP_ADMIN_ID}" --admin_password="${WP_ADMIN_PW}" --admin_email="hyojang@test.com" --skip-email --allow-root
 	wp user create "guest" "guest@test.com" --role=author --user_pass="guest" --allow-root
 fi
 
