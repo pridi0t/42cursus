25dd3a514567047285357e3212624ca38ff73d78
hyojang <hyojang@student.42seoul.kr>
2022-04-18 13:25:22 +0900

    debug done

--- inception/srcs/requirements/wordpress/conf/runtime_conf_copy.sh
+++ inception/srcs/requirements/wordpress/conf/runtime_conf_copy.sh
@@ -2,13 +2,6 @@
 
 FILE="/var/www/html/wordpress"
 
-if [ ! -e $FILE/index.php ]; then
-	cp -r /wordpress/* $FILE
-	cd /var/www/html/wordpress
-	sleep 14s;
-	wp config create --dbname="${MD_DBNAME}" --dbuser="${MD_ID}" --dbpass="${MD_PW}" --dbhost="${MD_HOST}" --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-	wp core install --url="https://hyojang.42.fr" --title="inception" --admin_user="${WP_ADMIN_ID}" --admin_password="${WP_ADMIN_PW}" --admin_email="hyojang@test.com" --skip-email --allow-root
-	wp user create "guest" "guest@test.com" --role=author --user_pass="guest" --allow-root
-fi
+nohup /runtime_wp_conf.sh &
 
 exec /usr/sbin/php-fpm7.3 -F -R
---
+++ inception/srcs/requirements/wordpress/conf/runtime_wp_conf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILE="/var/www/html/wordpress"
+
+if [ ! -e $FILE/index.php ]; then
+	cp -r /wordpress/* $FILE
+	cd /var/www/html/wordpress
+	sleep 14s;
+	wp config create --dbname="${MD_DBNAME}" --dbuser="${MD_ID}" --dbpass="${MD_PW}" --dbhost="${MD_HOST}" --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+fi
+
+wp core install --url="https://hyojang.42.fr" --title="inception" --admin_user="${WP_ADMIN_ID}" --admin_password="${WP_ADMIN_PW}" --admin_email="hyojang@test.com" --skip-email --allow-root
+wp user create "guest" "guest@test.com" --role=author --user_pass="guest" --allow-root
